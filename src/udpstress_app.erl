2f8d9cdefc3018bc08c338998323477797b32884
Amir Ghassemi Nasr <ameretat.reith@gmail.com>
2017-05-23 23:32:56 +0430

    procket send/recv methods

--- src/udpstress_app.erl
+++ src/udpstress_app.erl
@@ -37,7 +37,8 @@ start_app_from_args([ClientType, Server, StartPort, EndPort])
   when
     ClientType == "plain_client";
     ClientType == "genserver_client";
-    ClientType == "procket_client"
+    ClientType == "procket_client";
+    ClientType == "procket_send_client"
 ->
   start_app_from_args([ClientType, Server, "-i", undefined, StartPort, EndPort]);
 start_app_from_args([ClientType, Server, "-i", SendInterval, StartPort, EndPort]) ->
@@ -58,7 +59,8 @@ start_app_from_args([ServerType, StartPort, EndPort])
   when
     ServerType == "plain_server";
     ServerType == "genserver_server";
-    ServerType == "procket_server"
+    ServerType == "procket_server";
+    ServerType == "procket_recv_server"
 ->
   udpstress_server_sup:start_link(#{
                                     server_type => list_to_atom(ServerType),
@@ -67,6 +69,6 @@ start_app_from_args([ServerType, StartPort, EndPort])
                                   });
 start_app_from_args(Args) ->
   lager:error("invalid invocation: ~p~n", [lists:flatten(Args)]),
-  lager:error("usage: <genserver_client | plain_client | procket_client> [server_address] [-i send_interval] start_port end_port~n"),
-  lager:error("       <genserver_server | plain_server | procket_server> start_port end_port~n"),
+  lager:error("usage: <genserver_client | plain_client | procket_client | procket_send_client> [server_address] [-i send_interval] start_port end_port~n"),
+  lager:error("       <genserver_server | plain_server | procket_server | procket_recv_server> start_port end_port~n"),
   init:stop().
